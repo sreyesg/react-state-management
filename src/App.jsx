e839e0bd73fce8a80249fe9690e8b4ded2be4504
sreyesg <santiagoreyes.g@gmail.com>
2024-10-21 19:12:45 -0700

    define JSX using person state

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import {useState} from 'react'
 import './App.css';
 
 const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState(true)
+  const [isDarkMode, setIsDarkMode] = useState(false)
   console.log(isDarkMode)
   const [person, setUser] = useState({
     firstName: 'Mary',
     lastName: 'Moon',
-    hasPets: true,
+    hasPets: false,
     age: 24
   })
-  console.log(user)
+  console.log(person)
   
   return (
     <div className={isDarkMode ? 'dark':'light'}>
       <h1>Hello world!</h1>
+      <p>
+        Hello my name is {person.firstName}{person.lastName},
+        I am {person.age} years old,
+        and {person.hasPets ? 'Have':'Dont have'} pets.
+      </p>
       
     </div>
   );

03ea6e4086ca18d98e7003132f8727216a60e915
sreyesg <santiagoreyes.g@gmail.com>
2024-10-21 19:02:53 -0700

    Define person state

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import './App.css';
 const App = () => {
   const [isDarkMode, setIsDarkMode] = useState(true)
   console.log(isDarkMode)
+  const [person, setUser] = useState({
+    firstName: 'Mary',
+    lastName: 'Moon',
+    hasPets: true,
+    age: 24
+  })
+  console.log(user)
+  
   return (
     <div className={isDarkMode ? 'dark':'light'}>
       <h1>Hello world!</h1>

f3e310bf62cb872781feed86ca81e8f0e5842574
sreyesg <santiagoreyes.g@gmail.com>
2024-10-21 20:25:44 -0700

    Add handleMode function and refactor code state hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,36 @@ import {useState} from 'react'
 import './App.css';
 
 const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false)
-  console.log(isDarkMode)
-  const [person, setUser] = useState({
-    firstName: 'Mary',
-    lastName: 'Moon',
-    hasPets: false,
-    age: 24
-  })
-  console.log(person)
+
+  // const [person, setUser] = useState({
+    //   firstName: 'Mary',
+    //   lastName: 'Moon',
+    //   hasPets: false,
+    //   age: 24
+    // })
+    // console.log(person)  
+  const [mode, setMode] = useState("light")
+  const handleMode = (modeValue)=>{
+    console.log(modeValue)
+    setMode(modeValue)
+    console.log('this is mode', mode)
+  }
   
   return (
-    <div className={isDarkMode ? 'dark':'light'}>
-      <h1>Hello world!</h1>
-      <p>
-        Hello my name is {person.firstName}{person.lastName},
-        I am {person.age} years old,
-        and {person.hasPets ? 'Have':'Dont have'} pets.
-      </p>
-      
-    </div>
+    <>
+      <div className={mode}>
+        <h1>Hello world!</h1>
+        {/* <p>
+          Hello my name is {person.firstName}{person.lastName},
+          I am {person.age} years old,
+          and {person.hasPets ? 'Have':'Dont have'} pets.
+        </p> */}
+      </div>
+      <div>
+        <button onClick={() => handleMode('dark')}>Dark Mode</button>
+        <button onClick={() => handleMode('light')}>Light Mode</button>
+      </div>
+    </>  
   );
 };
 
